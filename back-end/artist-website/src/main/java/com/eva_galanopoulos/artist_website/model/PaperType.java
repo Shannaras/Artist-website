8f6f4a8e496e5d572051fd142b7766bcae278944
Shannaras <44708608+Shannaras@users.noreply.github.com>
2025-03-06 10:46:31 +0100

    PaperType table created in bdd

--- back-end/artist-website/src/main/java/com/eva_galanopoulos/artist_website/model/PaperType.java
+++ back-end/artist-website/src/main/java/com/eva_galanopoulos/artist_website/model/PaperType.java
@@ -1,5 +1,21 @@
 package com.eva_galanopoulos.artist_website.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class PaperType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_PaperType; 
+
+    private String name;
+
+    private String grain;    
 
 }
---
+++ back-end/artist-website/src/main/java/com/eva_galanopoulos/artist_website/repo/PaperTypeRepo.java
@@ -0,0 +1,9 @@
+package com.eva_galanopoulos.artist_website.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eva_galanopoulos.artist_website.model.PaperType;
+
+public interface PaperTypeRepo extends JpaRepository<PaperType, Long>{
+
+}
