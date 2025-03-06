6a8edf6f8a80e3baadfad4aeff9821de8eadefe4
Shannaras <44708608+Shannaras@users.noreply.github.com>
2025-03-06 10:23:30 +0100

    Exhibition Table created in bdd

--- back-end/artist-website/src/main/java/com/eva_galanopoulos/artist_website/model/Exhibition.java
+++ back-end/artist-website/src/main/java/com/eva_galanopoulos/artist_website/model/Exhibition.java
@@ -1,5 +1,29 @@
 package com.eva_galanopoulos.artist_website.model;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Exhibition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Date date;
+
+    private String location;
+
+    private boolean collaboration;
+
+    private String description;
 
 }
